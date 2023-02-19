d815565b35d87e1867ce126596e08fe6d78dcbe5
kinyanjui <01peterkinyanjui@gmail.com>
2023-02-19 14:10:51 -0800

    task1

--- shell.c
+++ shell.c
@@ -10,19 +10,43 @@ int main()
 {
 	size_t n = 0;
 	char *command = NULL;
-	char *argv[] = {"/bin/ls", NULL};
+	char **arr;//[];// = {command, NULL};
 	pid_t pid;
 	int step;
+	char *tkn;
+	int i;
+//	int status = system("command");
+	
 
 	while(1)
 	{
 		printf("$ ");
 		getline(&command,&n, stdin);
-		printf("%s, ",command);
+		tkn = strtok(command, "\n"); 
+		arr = malloc(sizeof(char *) * 32);
+		arr[0] = tkn;
+		int status = system(command);
+		{
+			if (status == -1)
+			{
+				printf("command not found.\n");
+			}
+		}		
+		if (strcmp(arr[0], "exit") == 0)
+		{
+			exit(0);
+		}
+		i = 1;
+		while (tkn != NULL)
+		{
+			tkn = strtok(NULL, "\n");
+			arr[i] = tkn;
+			i++;	
+		}
 		pid = fork();
 		if (pid == 0)
 		{
-			execve(argv[0], argv, NULL);
+			execve(arr[0], arr, NULL);
 		}
 //		if (pid == -1)
 //		{
--- shell.h
+++ shell.h
@@ -6,6 +6,8 @@
 #include <stdlib.h>
 #include <sys/types.h> 
 #include <sys/wait.h>
+#include <string.h>
+
 
 int execve(const char *pathname, char *const argv[],char *const envp[]);
 ssize_t getline(char **lineptr, size_t *n, FILE *stream);
