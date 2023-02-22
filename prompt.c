f0d69e150173aade7cfe7215d3468f441127346d
kinyanjui <01peterkinyanjui@gmail.com>
2023-02-22 12:47:37 -0800

    upto5

--- prompt.c
+++ prompt.c
@@ -1,1 +1,22 @@
+#include "shell.h"
+
+int main(void)
+{
+	size_t len = 0;
+	ssize_t read;
+	char *command = NULL;
+	char *argv[] = {command, NULL};
+
+	while (read)
+	{
+		write(1, "$ ", 2);
+		read = getline(&command, &len, stdin);
+		argv[0] = read;
+		printf("%s", command);
+		execve(argv[0], argv, NULL);
+	
+	}
+	free(command);
+	return (0);
+}
 
--- shell.c
+++ shell.c
@@ -14,45 +14,55 @@ int main()
 	pid_t pid;
 	int step;
 	char *tkn;
-	int i;
+	int i, fd;
+	ssize_t read;
+	char *newenviron[] = { NULL };
 //	int status = system("command");
 	
 
 	while(1)
 	{
-		printf("$ ");
-		getline(&command,&n, stdin);
-		tkn = strtok(command, "\n"); 
+//		printf("$ ");
+		write(1, "$ ", 2);
+		read = getline(&command,&n, stdin);
+//		printf("check -3\n");
+		tkn = strtok(command, " \n");
+
+//		printf("check -4");
 		arr = malloc(sizeof(char *) * 32);
+//		printf("check -2");
 		arr[0] = tkn;
 		int status = system(command);
 		{
 			if (status == -1)
 			{
 				printf("command not found.\n");
+				exit(0);
 			}
+//			printf("check -1");
 		}		
-		if (strcmp(arr[0], "exit") == 0)
+			if (strcmp(arr[0], "exit") == 0)
 		{
 			exit(0);
 		}
 		i = 1;
 		while (tkn != NULL)
 		{
-			tkn = strtok(NULL, "\n");
+			tkn = strtok(NULL, " \n");
+//			printf("second check1\n");
 			arr[i] = tkn;
 			i++;	
 		}
 		pid = fork();
 		if (pid == 0)
 		{
-			execve(arr[0], arr, NULL);
+			execve(arr[0], arr, newenviron);
 		}
-//		if (pid == -1)
-//		{
-//			perror("error");
-//		}
-		else
+		if (pid == -1)
+		{
+			perror("error");
+		}
+	else
 		{
 			wait(&step);
 		}
@@ -61,6 +71,7 @@ int main()
 	{
 		perror("error");
 	}
+	free(arr);
 	free(command);
 	return (0);
 }
